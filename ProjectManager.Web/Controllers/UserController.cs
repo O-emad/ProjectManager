951c309fecad0060fa8776376d1e1ac4e6736deb
Omar Rokiba <orokiba@sundevils.asu.edu>
2021-10-03 11:24:43 +0200

    starting identity

---
+++ ProjectManager.Web/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Web.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+    }
+}
